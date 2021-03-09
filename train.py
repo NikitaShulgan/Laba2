47d365f11a7acf78f7785b3bf4d7d6feccd130b2
NikitaShulgan <80168174+NikitaShulgan@users.noreply.github.com>
2021-03-09 22:55:14 +0300

    Update train.py

--- train.py
+++ train.py
@@ -16,6 +16,8 @@ from tensorflow.python.keras.callbacks import LearningRateScheduler
 from tensorflow.keras.applications import EfficientNetB0
 from tensorflow.keras.layers.experimental import preprocessing
 from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers
+
 
 
 # Avoid greedy memory allocation to allow shared GPU usage
