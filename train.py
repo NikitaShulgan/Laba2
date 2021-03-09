9d704d25de7dfe96f33aa2452ae667a33f0775fc
NikitaShulgan <80168174+NikitaShulgan@users.noreply.github.com>
2021-03-09 22:24:10 +0300

    Update train.py

--- train.py
+++ train.py
@@ -14,6 +14,8 @@ import time
 from tensorflow.python import keras as keras
 from tensorflow.python.keras.callbacks import LearningRateScheduler
 from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.layers.experimental import preprocessing
+
 
 # Avoid greedy memory allocation to allow shared GPU usage
 gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -59,7 +61,7 @@ def create_dataset(filenames, batch_size):
 
 def build_model():
   inputs = tf.keras.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
-  outputs = EfficientNetB0(include_top=False, weights='imagenet', classes=NUM_CLASSES)(inputs)
+  outputs = EfficientNetB0(include_top=False, input_tensor=x, weights='imagenet', classes=NUM_CLASSES)(inputs)
   return tf.keras.Model(inputs=inputs, outputs=outputs)
 
 
