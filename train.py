ce5849565f87af9a1864798628165cca79383036
NikitaShulgan <80168174+NikitaShulgan@users.noreply.github.com>
2021-03-15 11:53:47 +0300

    Update train.py

--- train.py
+++ train.py
@@ -27,7 +27,7 @@ for gpu in gpus:
 
 
 LOG_DIR = 'logs'
-BATCH_SIZE = 64
+BATCH_SIZE = 32
 NUM_CLASSES = 20
 RESIZE_TO = 224
 TRAIN_SIZE = 12786
@@ -64,7 +64,7 @@ def create_dataset(filenames, batch_size):
 
 def build_model():
   inputs = tf.keras.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
-  x = EfficientNetB0(include_top=False, weights="imagenet", pooling='max', classes=NUM_CLASSES)(inputs)
+  x = EfficientNetB0(include_top=False, weights="imagenet", pooling='avg', classes=NUM_CLASSES)(inputs)
   x.trainsble = False
   outputs = tf.keras.layers.Dense(NUM_CLASSES, activation = tf.keras.activations.relu)(x)
   return tf.keras.Model(inputs=inputs, outputs=outputs)
