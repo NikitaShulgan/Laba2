8fa636212d5efd231bfa4abef3a2b83a38dda600
NikitaShulgan <80168174+NikitaShulgan@users.noreply.github.com>
2021-03-15 16:27:06 +0300

    Update train.py

--- train.py
+++ train.py
@@ -73,11 +73,11 @@ def create_dataset(filenames, batch_size):
 
 def build_model():
   inputs = tf.keras.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
-  #x = img_augmentation(inputs)
-  x = EfficientNetB0(include_top=False, weights="imagenet", pooling='avg', classes=NUM_CLASSES)(inputs)
-  x.trainable = False
+  x = normalize(inputs)
+  model = EfficientNetB0(include_top=False, input_tensor=x, weights="imagenet", pooling='avg', classes=NUM_CLASSES)
+  model.trainable = False
   #x = layers.GlobalAveragePooling2D(name="avg_pool")(model)
-  outputs = tf.keras.layers.Dense(NUM_CLASSES, activation="softmax")(x)
+  outputs = tf.keras.layers.Dense(NUM_CLASSES, activation="softmax")(model)
   return tf.keras.Model(inputs=inputs, outputs=outputs)
 
 
