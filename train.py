1cddbf9b9f8dcf79093e6b90e819f707a4fe4bec
NikitaShulgan <80168174+NikitaShulgan@users.noreply.github.com>
2021-03-17 00:40:46 +0300

    Update train.py

--- train.py
+++ train.py
@@ -64,7 +64,7 @@ def create_dataset(filenames, batch_size):
 
 def build_model():
   inputs = tf.keras.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
-  outputs = EfficientNetB0(include_top=True, input_tensor=inputs, weights=None, classes=NUM_CLASSES)
+  outputs = EfficientNetB0(include_top=True, weights=None, classes=NUM_CLASSES)(inputs)
   return tf.keras.Model(inputs=inputs, outputs=outputs)
 
 
